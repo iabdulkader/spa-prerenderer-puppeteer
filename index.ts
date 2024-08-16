19664fbc2fe30627712a75d0ae958ddada050770
iabdulkader <abdulkadernow@gmail.com>
2024-08-17 03:23:10 +0600

    a

--- index.ts
+++ index.ts
@@ -21,7 +21,16 @@ async function renderPage(url: string): Promise<string> {
   try {
     browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-accelerated-2d-canvas",
+        "--no-first-run",
+        "--no-zygote",
+        "--single-process", // <- this one is important
+        "--disable-gpu",
+      ],
     });
 
     console.log("hitting url", url);
